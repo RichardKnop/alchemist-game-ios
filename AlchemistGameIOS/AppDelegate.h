d33fd41069c0691ac51ecaf2a6ad02cb95324987
Richard Knop <richard.knop@saffrondigital.com>
2013-09-01 19:04:40 +0100

    Initial Commit

---
+++ AlchemistGameIOS/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AlchemistGameIOS
+//
+//  Created by Richard Knop on 01/09/2013.
+//  Copyright (c) 2013 Richard Knop. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ AlchemistGameIOS/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  AlchemistGameIOS
+//
+//  Created by Richard Knop on 01/09/2013.
+//  Copyright (c) 2013 Richard Knop. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
